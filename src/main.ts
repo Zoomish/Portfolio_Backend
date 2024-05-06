d24abe2a17bd8bb6401503d7e492701ea91fde2f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-05-06 21:10:11 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT)
+    app.enableCors()
+    function aaa() {
+        setInterval(async () => {
+            await fetch('https://tg-bot-backend-61qy.onrender.com/loss')
+        }, 1000 * 60)
+    }
+    await app.listen(process.env.PORT, () => aaa())
 }
 bootstrap()
