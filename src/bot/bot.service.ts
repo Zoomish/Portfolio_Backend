8049274b6bdd72e8822aeb38eed9a54d6950beeb
Zoom <egor147536987@gmail.com>
2024-05-06 19:45:32 +0300

    "Added conditional check for /start command and response delay for bot"
    The diff includes changes to the 'message' event handler in the 'initBot' method of the 'BotService' class. A conditional check has been added

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -17,10 +17,22 @@ export class BotService implements OnModuleInit {
     async initBot(bot) {
         bot.on('message', async (msg) => {
             const chatId = msg.chat.id
-            bot.sendMessage(
-                chatId,
-                `Здравствуйте @${msg?.chat?.username}! Это мой бот(@Zoomish). Напишите мне любое сообщение и я отвечу вам в ближайшее время.`
+            const text = msg.text
+            if (text === '/start') {
+                await bot.sendMessage(
+                    chatId,
+                    `Здравствуйте @${msg?.chat?.username}! Это мой бот(@Zoomish). Напишите мне любое сообщение и я отвечу вам в ближайшее время.`
+                )
+            }
+            const msgWait = await bot.sendMessage(
+                msg.chat.id,
+                `Бот генерирует ответ...`
             )
+
+            setTimeout(async () => {
+                await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
+                await bot.sendMessage(msg.chat.id, msg.text)
+            }, 5000)
         })
     }
 }
