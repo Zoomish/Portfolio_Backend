0c24b02b7e473e218da8248204a181d9e9001607
Zoom <egor147536987@gmail.com>
2024-05-06 19:18:30 +0300

    Remove BotController, start implementing BotService with TelegramBot API
    The BotController class was removed from the bot module. The BotService class now has a constructor and starts importing the TelegramBot API. No functionality has been implemented yet

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common'
+import * as TelegramBot from 'telegram-bot-api'
 
 @Injectable()
-export class BotService {}
+export class BotService {
+    private bot: TelegramBot
+    constructor()
+}
