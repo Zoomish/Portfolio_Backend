820bc2245b84aa583091f901108cf74072eefcce
Zoom <egor147536987@gmail.com>
2024-05-06 19:33:34 +0300

    "Implement OnModuleInit in BotService to asyncly initialize the bot"
    This commit adds the OnModuleInit interface to the BotService class and moves the initialization of the bot to the onModuleInit method, changing it from synchronous to

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleInit } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import TelegramBot = require('node-telegram-bot-api')
 
 @Injectable()
-export class BotService {
-    constructor(private readonly configService: ConfigService) {
+export class BotService implements OnModuleInit {
+    constructor(private readonly configService: ConfigService) {}
+
+    async onModuleInit() {
         const telegramToken = this.configService.get('telegram.token')
         const bot = new TelegramBot(telegramToken, {
             polling: true,
         })
-        this.initBot(bot)
+        await this.initBot(bot)
     }
 
     async initBot(bot) {
