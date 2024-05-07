b11fa48ab04cf48faa6568733ef53c53b6581821
Zoom <egor147536987@gmail.com>
2024-05-07 14:22:59 +0300

    "Added project details to edit action's message in CallbackService"
    This commit adds the project's tags, live demo link, and repository link to the message that is sent when the 'edit' action is triggered in the Call

--- src/bot/services/callback.service.ts
+++ src/bot/services/callback.service.ts
@@ -10,9 +10,10 @@ export class CallbackService {
         if (action === 'edit') {
             const data = await this.projectService.findAll()
             await data.map(async (project) => {
+                console.log(project)
                 await bot.sendPhoto(msg.chat.id, `${project.image}`, {
                     parse_mode: 'html',
-                    caption: `<b>Название:</b> ${project.title}\n<b>Описание:</b> ${project.description}`,
+                    caption: `<b>Название:</b> ${project.title}\n<b>Описание:</b> ${project.description}\n<b>Теги:</b> ${project.tags}\n<b>Посмотреть:</b> <a href='${project.live}'>${project.live}</a>\n<b>Репозиторий:</b> <a href='${project.repository}'>${project.repository}</a>`,
                 })
             })
         }
