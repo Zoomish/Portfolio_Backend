5f95799187814b846f3d31c1fa42c1e9c97d08b6
Zoom <egor147536987@gmail.com>
2024-05-09 14:23:25 +0300

    "Return the result of data.map instead of awaiting an array of promises"
    Explanation:
    The code diff shows a change in the way the `data.map` method is called in the `CallbackService` class.

--- src/bot/services/callback.service.ts
+++ src/bot/services/callback.service.ts
@@ -18,7 +18,7 @@ export class CallbackService {
         if (action === 'projects') {
             const data = await this.projectService.findAll()
             await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-            await data.map(async (project) => {
+            return await data.map(async (project) => {
                 await bot.sendPhoto(msg.chat.id, `${project.image}`, {
                     parse_mode: 'html',
                     caption: `<b>Название:</b> ${project.title}\n<b>Описание:</b> ${project.description}\n<b>Теги:</b> ${project.tags}\n<b>Посмотреть:</b> <a href='${project.live}'>${project.live}</a>\n<b>Репозиторий:</b> <a href='${project.repository}'>${project.repository}</a>`,
@@ -30,7 +30,7 @@ export class CallbackService {
             const user = data[0]
             const work = user.work.replace(' ', '').split(',')
             await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
-            await bot.sendPhoto(msg.chat.id, `${user.image}`, {
+            return await bot.sendPhoto(msg.chat.id, `${user.image}`, {
                 parse_mode: 'html',
                 caption: `<b>Меня зовут:</b> ${user.name}\n<b>Мой email:</b> ${user.email}\n<b>Мой Гитхаб:</b> ${user.github}\n<b>Работаю в:</b> <a href='${work[0]}'>${work[1]}</a>\n<b>Мое портфолио:</b> <a href='${user.portfolio}'>${user.portfolio}</a>`,
                 reply_markup: {
