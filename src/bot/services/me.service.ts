c347e30a6e030b5670158b9726d0bc6660a1863e
Zoom <egor147536987@gmail.com>
2024-05-10 16:53:20 +0300

    "Add methods to calculate user's experience in years and months; update caption in getMe method"

--- src/bot/services/me.service.ts
+++ src/bot/services/me.service.ts
@@ -8,11 +8,16 @@ export class MeService {
         const msgWait = await bot.sendMessage(msg.chat.id, `Получаю данные...`)
         const data1 = await this.userService.findAll()
         const user = data1[0]
+        const expirience = Math.ceil(
+            (new Date(new Date().toISOString().split('T')[0]).getTime() -
+                new Date(user.expirience).getTime()) /
+                (1000 * 60 * 60 * 24 * 30)
+        )
         const work = user.work.replace(' ', '').split(',')
         await bot.deleteMessage(msgWait.chat.id, msgWait.message_id)
         return await bot.sendPhoto(msg.chat.id, `${user.image}`, {
             parse_mode: 'html',
-            caption: `<b>Меня зовут:</b> ${user.name}\n<b>Мой email:</b> ${user.email}\n<b>Мое портфолио:</b> <a href='${user.portfolio}'> Сайт визитка</a>\n<b>Мой Гитхаб:</b> ${user.github}\n<b>Мой опыт работы:</b> ${(new Date(new Date().toISOString().split('T')[0]).getTime() - new Date(user.expirience).getTime()) / (1000 * 60 * 60 * 24)}\n<b>Работаю в:</b> <a href='${work[0]}'>${work[1]}</a>\n<b>Мои навыки:</b> \n${user.skills
+            caption: `<b>Меня зовут:</b> ${user.name}\n<b>Мой email:</b> ${user.email}\n<b>Мое портфолио:</b> <a href='${user.portfolio}'> Сайт визитка</a>\n<b>Мой Гитхаб:</b> ${user.github}\n<b>Мой опыт работы:</b> ${Math.floor(expirience / 12)} ${await this.age(Math.floor(expirience / 12))} ${expirience % 12} ${await this.month(expirience % 12)}\n<b>Работаю в:</b> <a href='${work[0]}'>${work[1]}</a>\n<b>Мои навыки:</b> \n${user.skills
                 .replace(' ', '')
                 .split(',')
                 .map((skill) => {
@@ -30,5 +35,34 @@ export class MeService {
                 ],
             },
         })
+    }
+    async age(age) {
+        let txt
+        let count = age % 100
+        if (count >= 5 && count <= 20) {
+            txt = 'лет'
+        } else {
+            count = count % 10
+            if (count == 1) {
+                txt = 'год'
+            } else if (count >= 2 && count <= 4) {
+                txt = 'года'
+            } else {
+                txt = 'лет'
+            }
+        }
+        return txt
+    }
+    async month(month) {
+        switch (month) {
+            case month == 1:
+                return 'месяц'
+            case month > 1 && month < 5:
+                return 'месяца'
+            case month > 4:
+                return 'месяцев'
+            default:
+                break
+        }
     }
 }
