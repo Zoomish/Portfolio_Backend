035bce56c50e197a8a19c021e0d56a10fc42f5a1
Zoom <egor147536987@gmail.com>
2024-05-17 09:17:06 +0300

    "Rename UserController to SkillController and UserService to SkillService in skill.module.ts"

--- src/skill/skill.module.ts
+++ src/skill/skill.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common'
-import { UserController } from './skill.controller'
-import { UserService } from './skill.service'
+import { SkillController } from './skill.controller'
+import { SkillService } from './skill.service'
 import { FilesModule } from 'src/files/files.module'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { User } from './model/skill.model'
 
 @Module({
     imports: [FilesModule, SequelizeModule.forFeature([User])],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: [UserService],
+    controllers: [SkillController],
+    providers: [SkillService],
 })
-export class UserModule {}
+export class SkillModule {}
