03765f9706df6d7c2259033b31b0117dd47d70a5
Zoom <egor147536987@gmail.com>
2024-05-17 09:20:26 +0300

    "Added Skill model to User model and User module, and updated User model to include HasMany relationship with Skill"

--- src/user/model/user.model.ts
+++ src/user/model/user.model.ts
@@ -1,4 +1,5 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript'
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript'
+import { Skill } from 'src/skill/model/skill.model'
 
 interface UserCreationAttrs {
     name: string
@@ -35,7 +36,7 @@ export class User extends Model<User, UserCreationAttrs> {
     @Column({ type: DataType.STRING, allowNull: false })
     work: string
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @HasMany(() => Skill)
     skills: string
 
     @Column({ type: DataType.STRING, allowNull: false })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserService } from './user.service'
 import { FilesModule } from 'src/files/files.module'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { User } from './model/user.model'
+import { Skill } from 'src/skill/model/skill.model'
 
 @Module({
-    imports: [FilesModule, SequelizeModule.forFeature([User])],
+    imports: [FilesModule, SequelizeModule.forFeature([User, Skill])],
     controllers: [UserController],
     providers: [UserService],
     exports: [UserService],
