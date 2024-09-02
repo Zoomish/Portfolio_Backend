04a4f051c8e18be11f57e6ef541cd782e4beed9b
Zoom <egor147536987@gmail.com>
2024-09-02 18:00:21 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors()
     function aaa() {
-        setInterval(async () => {
-            await fetch(
-                'https://portfolio-backend-8h15.onrender.com/projects/AAA'
-            )
-        }, 1000 * 60 * 14 + 1000 * 30)
+        setInterval(
+            async () => {
+                await fetch(
+                    'https://portfolio-backend-8h15.onrender.com/projects/AAA'
+                )
+            },
+            1000 * 60 * 14 + 1000 * 30
+        )
     }
     await app.listen(process.env.PORT, () => aaa())
 }
