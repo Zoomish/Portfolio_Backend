2788c9db24e50219a64577d91dd2afd2c02a46f5
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-10-10 21:10:08 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    app.enableCors()
+    app.enableCors({
+        origin: 'https://portfolio-zoomish.vercel.app',
+        credentials: true,
+        methods: ['GET'],
+        allowedHeaders: [
+            'Origin',
+            'X-Requested-With',
+            'Content-Type',
+            'Accept',
+        ],
+    })
     function aaa() {
         setInterval(
             async () => {
